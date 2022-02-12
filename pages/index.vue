27435e3217064e0328518aad44a95dfea8d94b77
Raymond Chai <csechuan@gmail.com>
2022-02-12 17:37:26 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Tutorial />
+</template>
+
+<script setup>
+// import Vue from 'vue'
+// import VueCompositionAPI from '@vue/composition-api'
+// Vue.use(VueCompositionAPI)
+
+// import {
+//   getCurrentInstance
+// } from '@vue/composition-api'
+
+//  const instance = getCurrentInstance() 
+//   const vm = instance?.proxy || instance
+//   console.log(vm)
+
+
+import useSWRV from "swrv";
+const endpoint = "https://jsonplaceholder.typicode.com/users";
+const { data, error } = useSWRV(endpoint, undefined);
+console.log(`data ${JSON.stringify(data)} error ${JSON.stringify(error)}`);
+</script>
